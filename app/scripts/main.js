caad27d9b84c41a05e62aa7bd5a10acbc054bc2f
progbass <progbass@gmail.com>
2017-01-06 19:49:29 -0600

    Layout Updates 6.1.17

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,22 @@
 (function($){
 	$(window).scroll(function(){
-
-		var document_top = $(window).scrollTop() - $('#news_module').offset().top;
-		//console.log(document_top)
+		var window_scroll = $(window).scrollTop();
+		var header_position = $('header').offset().top;
+		var header_height = header_position + $('header').height();
+		//console.log(window_scroll+'  :  '+header_height)
 		//$('#news_module .wrapper').css('top', document_top+'px')
+		//
+		if(window_scroll >= header_height){
+			$('#main_menu').toggleClass('fixed', true)
+		} else {
+			$('#main_menu').toggleClass('fixed', false)
+		}
+	});
+
+
+	$('#main_menu .menu_icon').click(function(e){
+		$('#mobile_menu').toggleClass('visible');
+		$('#main_menu #search_frm').hide(false);
+		return false;
 	});
 })(jQuery);
