2e572c84d33d5092d77c7076c8ef3818340346e9
progbass <progbass@gmail.com>
2017-01-13 20:10:11 -0600

    Updates 12.01.17
    Comments, fixes, Search & Newsletter

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,58 +1,54 @@
 (function($){
+	// Main Variables
 	var mobile_mode = true;
 	var bp_desktop = 900;
 	var menu_open = false;
 	var menu_hide_interval = 0;
 	var menu_page_lastPos = 0;
+	var search_open = false;
 
 
+	/*----------------------------------------------------
+	//	FUNCTIONS
+	----------------------------------------------------*/
+	// Window Scroll
 	function onScroll(){
+		// Scroll Variables
 		var window_scroll = $(window).scrollTop();
+
+		////////////////////
+		// MOBILE MENU 
+		// Get header position and height
 		var header_position = $('header').offset().top;
 		var header_height = header_position + $('header .photo_holder').height();
 
-		//
-		if(!menu_open)
-			menu_page_lastPos = window_scroll;
-
-
-		//
+		// If mobile menu is hidden
 		if(!menu_open){
+			// If scroll has passed header position, show fixed menu
 			if(window_scroll >= header_height){
 				$('#main_menu').toggleClass('fixed', true)
 			} else {
 				$('#main_menu').toggleClass('fixed', false)
 			}
 		}
+
+
+		////////////////////
+		// MOBILE MENU
+		// If mobile menu is hidden, update scroll last position
+		if(!menu_open)
+			menu_page_lastPos = window_scroll;
+
 	}
-	$(window).scroll(onScroll);
 
 
-	$(window).on('resize', function(){
-		//
-		$('img').each(function() {
-			updateImagesRatios($(this));
-		});
-
-		//
-		if( $(window).innerWidth >= bp_desktop){
-			mobile_mode = false;
-		} else {
-			mobile_mode = true;
-		}
-
-		//
-		toggleMobileMenu(false);
-	});
-
-
-
+	// UPDATE IMAGE ASPECT RATIOS
 	function updateImagesRatios(_target){
-		//
+		// Get ratios of container and image iself
 		var holder_ratio = _target.parent().height() / _target.parent().width();
 		var image_ratio = _target.height() / _target.width();
 
-	    
+	    // Compare ratios and fix image layout
 	    if(holder_ratio > image_ratio) {
 			_target.toggleClass('vertical', true);
 	    } else {
@@ -61,6 +57,88 @@
 	}
 
 
+	// OPEN / CLOSE MOBILE MENU
+	function toggleMobileMenu(_flag){
+		// Update mobile menu flag
+		menu_open = _flag;
+
+		// Change styles of menu elements
+		$('#mobile_menu').toggleClass('visible', menu_open);
+		$('#main_menu #search_frm').toggle(!menu_open);
+		$('#main_menu').toggleClass('mobileMenu', menu_open);
+
+		// If mobile menu is open
+		if(menu_open){
+			// Update menu styles
+			$('#main_menu').removeClass('fixed').css('position', 'fixed');
+			
+			// Hide website content to prevent scrolling
+			menu_hide_interval = setInterval(function(){
+				// Hide all but menu
+				$('#main_container > :not(#main_menu)').css({
+					'visibility': 'hidden',
+				});
+			}, 1000);
+
+
+		// If mobile menu is hidden
+		} else {
+			// prevent hidding website content
+			clearInterval( menu_hide_interval );
+
+			// Show website content
+			$('#main_container > :not(#main_menu)').css({
+				'visibility': 'visible',
+			});
+
+			// Scroll to last postition
+			$(window).scrollTop(menu_page_lastPos);
+			onScroll();
+		}
+	}
+
+	// OPEN / CLOSE SEARCH LIGHTBOX
+	function toggleSearchWindow( _target ){
+		search_open = _target;
+		$('#search_module').toggleClass('visible', search_open);
+	}
+
+
+
+
+
+	/*----------------------------------------------------
+	//	WINDOW EVENTS
+	----------------------------------------------------*/
+	// SCROLL
+	$(window).scroll(onScroll);
+
+	// RESIZE
+	$(window).on('resize', function(){
+		// Update Image Ratios
+		$('img').each(function() {
+			updateImagesRatios($(this));
+		});
+
+		// Update App Mode
+		if( $(window).innerWidth >= bp_desktop){
+			mobile_mode = false;
+		} else {
+			mobile_mode = true;
+		}
+
+		// Hide Mobile Menu
+		toggleMobileMenu(false);
+	});
+
+
+
+	
+
+	/*----------------------------------------------------
+	//	MOBILE MENU
+	----------------------------------------------------*/
+	// MENU ICON BUTTON
 	$('#main_menu .menu_icon').click(function(e){
 		//menu_open = !menu_open;
 		toggleMobileMenu(!menu_open);
@@ -69,42 +147,38 @@
 		return false;
 	});
 
-	function toggleMobileMenu(_flag){
-		var flag = _flag;
-		menu_open = flag;
-
-		$('#mobile_menu').toggleClass('visible', menu_open);
-		$('#main_menu #search_frm').toggle(!menu_open);
-		$('#main_menu').toggleClass('mobileMenu', menu_open);
-
-		if(menu_open){
-			$('#main_menu').removeClass('fixed').css('position', 'fixed');
-			
-			menu_hide_interval = setInterval(function(){
-				$('#main_container > :not(#main_menu)').css({
-					'visibility': 'hidden',
-				});
-
-				//$('#main_container > :not(#main_menu)').hide();
-			}, 1000);
-
-		} else {
-			clearInterval( menu_hide_interval );
-			// $('#main_container').css({
-			// 	'overflow': 'visible',
-			// 	'height': 'auto'
-			// });
-			$('#main_container > :not(#main_menu)').css({
-				'visibility': 'visible',
-			});
-			$(window).scrollTop(menu_page_lastPos);
-			onScroll();
-		}
-	}
 
 
-	$('img').on('load', function() {
+
+	/*----------------------------------------------------
+	//	MAIN MENU
+	----------------------------------------------------*/
+	// Open Search Lightbox
+	$('a.searchWindowOpen_btn').click(function(_e){
+		_e.preventDefault();
+		toggleSearchWindow(!search_open);
+	});
+	$('#search_module .overlay').click(function(_e){
+		_e.preventDefault();
+		toggleSearchWindow(false);
+	});
+
+	
+
+	/*----------------------------------------------------
+	//	RESPONSIVE / PROPORTIONAL IMAGES
+	----------------------------------------------------*/
+	// Hide images and add event listener
+	$('img')
+	//.css({ 'visibility': 'hidden'})
+	//.fadeOut(0)
+	.on('load', function() {
+		// Update image aspect after load
 		updateImagesRatios($(this));
+
+		// Show image
+		//$(this).css({ 'visibility': 'visible'})
+		//.fadeIn();
 	});
 
 })(jQuery);
